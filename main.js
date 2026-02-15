f0f84c1e46de6f58f17cb471e02fcacded45ac87
Shakhzoda2002 <159199868+Shakhzoda2002@users.noreply.github.com>
2026-02-15 14:02:42 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleBtn = document.getElementById("toggleSkillsBtn");
-  const skillsContent = document.getElementById("skillsContent");
-
-  toggleBtn.addEventListener("click", function () {
-    if (skillsContent.style.display === "none") {
-      skillsContent.style.display = "block";
-      toggleBtn.textContent = "Hide Skills";
-    } else {
-      skillsContent.style.display = "none";
-      toggleBtn.textContent = "Show Skills";
-    }
-  });
-});
+console.log("main.js is connected successfully!");
